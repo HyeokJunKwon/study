181c0be0127d09a48b08481359f989ea4cb752cd
sentunia <rgwgun@gmail.com>
2017-10-15 12:36:03 +0900

    please..

---
+++ src/Test.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		String text = sc.nextLine();
+		int cnt = 1;
+		String temp = "";
+		String[] arr = text.split("");
+		
+		for (int i = 0; i < text.length(); i++) {
+//			sb.append(arr[i]);
+//			sb.append(cnt);
+			if (i == text.length() - 1) break;
+			if (arr[i].equals(arr[i + 1])) {
+				cnt++;
+//				temp = arr[i];
+			} else if (arr[i + 1] != "") {
+				sb.append(arr[i]);
+				sb.append(cnt);
+			} else {
+				sb.append(arr[i]);
+				sb.append(cnt);
+			}
+		}
+		
+		sc.close();
+//		if (sb.length() > text.length()) {
+//			System.out.println(text);
+//		} else {
+			System.out.println(sb);
+//		}
+	}
+}
