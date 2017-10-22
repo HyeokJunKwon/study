8ca04654bfedb2e433fd4e870a0d7b35d08c6ce9
sentunia <rgwgun@gmail.com>
2017-10-22 10:08:47 +0900

    ...

---
+++ src/ch02/Q2_4_1.java
@@ -0,0 +1,47 @@
+package ch02;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Q2_4_1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Input    : ");
+		// ex) 1 2 3 4 5
+		String input = sc.nextLine();
+		System.out.print("Division : ");
+		// ex) 3
+		int x = Integer.parseInt((sc.nextLine()));
+		
+		sc.close();
+		
+		System.out.println("Output   : " + listDivision(input, x));
+	}
+	
+	private static LinkedList<Integer> listDivision(String input, int x) {
+		LinkedList<Integer> inputList = new LinkedList<>();
+		LinkedList<Integer> outputList = new LinkedList<>();
+		
+		String [] arr = input.split(" ");
+		
+		for (int i = 0; i < arr.length; i++) {
+			inputList.add(Integer.parseInt(arr[i]));
+		}
+		
+		for (int i = 0; i < inputList.size(); i++) {
+			if (inputList.get(i) < x) {
+				outputList.addFirst(inputList.get(i));
+			} else {
+				outputList.addLast(inputList.get(i));
+			}
+		}
+		
+//		Collections.sort(inputList);
+//		return inputList;
+		return outputList;
+	}
+}
+
+
+
+
